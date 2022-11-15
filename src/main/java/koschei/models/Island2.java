7d64a4e76eb9b007c5e3375fad2857e6098f0273
Dina Zainetdinova <zntdnvdn@gmail.com>
2022-11-15 19:10:17 +0500

    first commit

--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -1,8 +1,12 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+
 public class Island2 {
 
     private Wood3 wood;
+
 
     public Island2(Wood3 wood) {
         this.wood = wood;
